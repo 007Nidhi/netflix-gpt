64e1e1668d984ea7a987def8430a1700a64b9357
nidhi <global>
2024-05-04 15:01:39 +0530

    first code pu

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacttttttttttttttttttttttttttttttttttttttt
+          Learn Git
         </a>
       </header>
     </div>
